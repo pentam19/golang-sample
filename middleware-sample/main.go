aba1ee564f9a38935fe126191a94f0f204d5c084
masahirot <13981959+pentam19@users.noreply.github.com>
2019-05-12 17:34:22 +0900

    add group pattern

--- middleware-sample/main.go
+++ middleware-sample/main.go
@@ -21,16 +21,20 @@ func main() {
 	sampleGroup2 := sampleGroup.Group("/")
 
 	// path: /test/path1
-	// Middleware実行: されない
+	// Middleware1: 実行されない
+	// Middleware2: 実行されない
+	// Middleware3: 実行されない
 	sampleGroup.GET("/path1", func(c *gin.Context) {
 		ctx := appengine.NewContext(c.Request)
 		log.Infof(ctx, "path1 logic")
 		c.String(200, "path1 !!!")
 	})
-	sampleGroup.Use(sampleMiddleware())
+	sampleGroup.Use(sampleMiddleware1())
 	{
 		// path: /test/path2
-		// Middleware実行: される
+		// Middleware1: 実行される
+		// Middleware2: 実行されない
+		// Middleware3: 実行されない
 		sampleGroup.GET("/path2", func(c *gin.Context) {
 			ctx := appengine.NewContext(c.Request)
 			log.Infof(ctx, "path2 logic")
@@ -38,18 +42,47 @@ func main() {
 		})
 	}
 	// path: /test/path3
-	// Middleware実行: される
+	// Middleware1: 実行される
+	// Middleware2: 実行されない
+	// Middleware3: 実行されない
 	sampleGroup.GET("/path3", func(c *gin.Context) {
 		ctx := appengine.NewContext(c.Request)
 		log.Infof(ctx, "path3 logic")
 		c.String(200, "path3 !!!")
 	})
 	// path: /test/path4
-	// Middleware実行: されない
+	// Middleware1: 実行されない (Use(sampleMiddleware1())の前にGroup作ったため)
+	// Middleware2: 実行されない
+	// Middleware3: 実行されない
 	sampleGroup2.GET("/path4", func(c *gin.Context) {
 		ctx := appengine.NewContext(c.Request)
 		log.Infof(ctx, "path4 logic")
 		c.String(200, "path4 !!!")
+	})
+
+	sampleGroup3 := sampleGroup2.Group("/")
+	sampleGroup3.Use(sampleMiddleware2())
+	// path: /test/path5
+	// Middleware1: 実行されない (Use(sampleMiddleware1())の前にGroup作ったため)
+	// Middleware2: 実行される
+	// Middleware3: 実行されない
+	sampleGroup3.GET("/path5", func(c *gin.Context) {
+		ctx := appengine.NewContext(c.Request)
+		log.Infof(ctx, "path5 logic")
+		c.String(200, "path5 !!!")
+	})
+
+	sampleGroup4 := sampleGroup3.Group("/")
+	sampleGroup4.Use(sampleMiddleware1())
+	sampleGroup4.Use(sampleMiddleware3())
+	// path: /test/path6
+	// Middleware1: 実行される
+	// Middleware2: 実行される (Use(sampleMiddleware2())が引き継がれている)
+	// Middleware3: 実行される
+	sampleGroup4.GET("/path6", func(c *gin.Context) {
+		ctx := appengine.NewContext(c.Request)
+		log.Infof(ctx, "path6 logic")
+		c.String(200, "path6 !!!")
 	})
 
 	http.Handle("/", r)
@@ -61,27 +94,59 @@ func main() {
 		[GIN-debug] GET    /test/path2               --> main.main.func2 (2 handlers)
 		[GIN-debug] GET    /test/path3               --> main.main.func3 (2 handlers)
 		[GIN-debug] GET    /test/path4               --> main.main.func4 (1 handlers)
-		INFO     2019-05-12 07:39:15,351 instance.py:294] Instance PID: 52004
-		2019/05/12 07:40:35 INFO: path1 logic
-		INFO     2019-05-12 07:40:35,539 module.py:861] default: "GET /test/path1 HTTP/1.1" 200 9
-		2019/05/12 07:40:42 INFO: before logic
-		2019/05/12 07:40:42 INFO: path2 logic
-		2019/05/12 07:40:42 INFO: after logic
-		INFO     2019-05-12 07:40:42,022 module.py:861] default: "GET /test/path2 HTTP/1.1" 200 9
-		2019/05/12 07:40:49 INFO: before logic
-		2019/05/12 07:40:49 INFO: path3 logic
-		2019/05/12 07:40:49 INFO: after logic
-		INFO     2019-05-12 07:40:49,377 module.py:861] default: "GET /test/path3 HTTP/1.1" 200 9
-		2019/05/12 07:41:00 INFO: path4 logic
-		INFO     2019-05-12 07:41:00,952 module.py:861] default: "GET /test/path4 HTTP/1.1" 200 9
+		[GIN-debug] GET    /test/path5               --> main.main.func5 (2 handlers)
+		[GIN-debug] GET    /test/path6               --> main.main.func6 (4 handlers)
+		INFO     2019-05-12 08:26:19,980 instance.py:294] Instance PID: 52732
+		2019/05/12 08:26:27 INFO: path1 logic
+		INFO     2019-05-12 08:26:27,499 module.py:861] default: "GET /test/path1 HTTP/1.1" 200 9
+		2019/05/12 08:26:29 INFO: [Middleware 1] before logic
+		2019/05/12 08:26:29 INFO: path2 logic
+		2019/05/12 08:26:29 INFO: [Middleware 1] after logic
+		INFO     2019-05-12 08:26:29,688 module.py:861] default: "GET /test/path2 HTTP/1.1" 200 9
+		2019/05/12 08:26:31 INFO: [Middleware 1] before logic
+		2019/05/12 08:26:31 INFO: path3 logic
+		2019/05/12 08:26:31 INFO: [Middleware 1] after logic
+		INFO     2019-05-12 08:26:31,817 module.py:861] default: "GET /test/path3 HTTP/1.1" 200 9
+		2019/05/12 08:26:34 INFO: path4 logic
+		INFO     2019-05-12 08:26:34,061 module.py:861] default: "GET /test/path4 HTTP/1.1" 200 9
+		2019/05/12 08:26:36 INFO: [Middleware 2] before logic
+		2019/05/12 08:26:36 INFO: path5 logic
+		2019/05/12 08:26:36 INFO: [Middleware 2] after logic
+		INFO     2019-05-12 08:26:36,492 module.py:861] default: "GET /test/path5 HTTP/1.1" 200 9
+		2019/05/12 08:26:38 INFO: [Middleware 2] before logic
+		2019/05/12 08:26:38 INFO: [Middleware 1] before logic
+		2019/05/12 08:26:38 INFO: [Middleware 3] before logic
+		2019/05/12 08:26:38 INFO: path6 logic
+		2019/05/12 08:26:38 INFO: [Middleware 3] after logic
+		2019/05/12 08:26:38 INFO: [Middleware 1] after logic
+		2019/05/12 08:26:38 INFO: [Middleware 2] after logic
+		INFO     2019-05-12 08:26:38,790 module.py:861] default: "GET /test/path6 HTTP/1.1" 200 9
 	*/
 }
 
-func sampleMiddleware() gin.HandlerFunc {
+func sampleMiddleware1() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := appengine.NewContext(c.Request)
-		log.Infof(ctx, "before logic")
+		log.Infof(ctx, "[Middleware 1] before logic")
 		c.Next()
-		log.Infof(ctx, "after logic")
+		log.Infof(ctx, "[Middleware 1] after logic")
+	}
+}
+
+func sampleMiddleware2() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := appengine.NewContext(c.Request)
+		log.Infof(ctx, "[Middleware 2] before logic")
+		c.Next()
+		log.Infof(ctx, "[Middleware 2] after logic")
+	}
+}
+
+func sampleMiddleware3() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := appengine.NewContext(c.Request)
+		log.Infof(ctx, "[Middleware 3] before logic")
+		c.Next()
+		log.Infof(ctx, "[Middleware 3] after logic")
 	}
 }
